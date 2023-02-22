f7ba83b536f2d5fb367923e63ae1432b8b760ee6
Juarez Martins <juniorbmx13@gmail.com>
2023-02-22 19:07:25 -0300

    refactor: class Main - fix: remove method NullPointer

--- src/main/java/org/estrutura/Main.java
+++ src/main/java/org/estrutura/Main.java
@@ -23,14 +23,16 @@ public class Main {
         minhaArvore.exibirPreOrdem();
         minhaArvore.exibirPosOrdem();
 
+
+        System.out.println();
         System.out.println("\nAgora vamos manipular os dados.\n");
 
         int num = 0;
         while (num != 4) {
-
+            System.out.println();
             System.out.println("Digite 1 - Exibição de Dados \n" +
                     "Digite 2 - Inserir Dados \nDigite 3 - Remover Dados" +
-                    "Digite 4 - sair");
+                    "\nDigite 4 - sair");
             num = scan.nextInt();
             if (num == 1) {
                 System.out.println("Digite 1 - exibir dados InOrdem\nDigite 2 - exibir dados PreOrdem" +
@@ -38,13 +40,16 @@ public class Main {
                 int numTemp = scan.nextInt();
                 if (numTemp == 1) {
                     minhaArvore.exibirInOrdem();
+                    System.out.println();
                 }
                 if (numTemp == 2) {
                     minhaArvore.exibirPreOrdem();
+                    System.out.println();
                 }
-                if (numTemp == 1) {
+                if (numTemp == 3) {
                     minhaArvore.exibirPosOrdem();
-                } else {
+                    System.out.println();
+                }if(numTemp > 3 && numTemp < 1) {
                     System.out.println("Número inválido!");
                 }
             }
@@ -56,11 +61,14 @@ public class Main {
                 minhaArvore.exibirInOrdem();
             }
             if (num == 3) {
+                System.out.print("Digite o número inteiro a ser retirado: ");
                 int numTemp = scan.nextInt();
-                minhaArvore.remover(new Obj(numTemp));
+                Obj tempObj = new Obj(numTemp);  //obj temporário criado para comparar na pesquisa
+                minhaArvore.remover(tempObj);
                 System.out.println("Procure o número retirado em ordem numérica.");
                 minhaArvore.exibirInOrdem();
-            }else{
+
+            }if(num > 3 && num < 1) {
                 System.out.println("Número de operação inválido!");
             }
         }
